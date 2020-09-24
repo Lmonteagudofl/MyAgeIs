e5b43dd7c22cd2323cdd81615d647033aa207d45
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2020-09-24 17:08:13 +0000

    i stated what my age is and i put a system out put of i am + my age + years old.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 16;
+    System.out.println( "i am" + myAge + "years old.");
     
   }
 }
